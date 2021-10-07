2e416bc5331c6dcac383a6a8f263a2abfdbf0e62
Mickael Lecoq <mickael@lecoq.me>
2021-10-07 14:44:34 +0200

    Add case for orientation issue

--- PdfScreen.js
+++ PdfScreen.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PSPDFKitView from '@archireport/react-native-pspdfkit';
 import {View} from 'react-native';
 
 export default function PdfScreen() {
+  const [positions, setPositions] = useState([]);
   return (
     <View style={{flex: 1, backgroundColor: 'red'}}>
       <PSPDFKitView
@@ -13,6 +14,28 @@ export default function PdfScreen() {
         }}
         style={{flex: 1, backgroundColor: 'blue'}}
         document="file:///android_asset/report.pdf"
+        positionTitle={`${positions.length}`}
+        positions={positions}
+        onPositionChanged={({nativeEvent}) => {
+          switch (nativeEvent.action) {
+            case 'CREATED': {
+              setPositions(pos =>
+                pos.concat({
+                  ...nativeEvent.position,
+                  id: `pos${pos.length + 1}`,
+                }),
+              );
+              break;
+            }
+            case 'UPDATED': {
+              setPositions(pos =>
+                pos.map(p =>
+                  p.id === nativeEvent.position.id ? nativeEvent.position : p,
+                ),
+              );
+            }
+          }
+        }}
       />
     </View>
   );
